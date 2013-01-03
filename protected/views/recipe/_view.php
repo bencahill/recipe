4a92dec44797013956fd2a36f89b2861e4d8d632
Benjamin Cahill <ben@benjamincahill.com>
2013-01-03 08:05:41 -0600

    Make recipe index links smaller

--- protected/views/recipe/_view.php
+++ protected/views/recipe/_view.php
@@ -1,6 +1,6 @@
 <div class="view">
 
-	<h3><?php echo CHtml::link(CHtml::encode($data->title), array('view', 'id'=>$data->id)); ?></h3>
+	<h4><?php echo CHtml::link(CHtml::encode($data->title), array('view', 'id'=>$data->id)); ?></h4>
 
 	<?php echo CHtml::encode($data->description); ?>
 
